c1b93f6cf7ca2d51e5f1c6db41a5f05b0dff3278
adalberto-g <alebx32@gmail.com>
2017-11-01 19:58:31 -0600

    make car avoid colision by slowing down

--- src/main.cpp
+++ src/main.cpp
@@ -238,7 +238,8 @@ int main() {
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
-          	auto sensor_fusion = j[1]["sensor_fusion"];
+            //auto sensor_fusion = j[1]["sensor_fusion"];
+            vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
           
             int prev_size = previous_path_x.size();
           
@@ -248,6 +249,39 @@ int main() {
             double ref_y;
             double ref_yaw;
           
+            //sensor fusion
+            if(prev_size > 0)
+            {
+              car_s = end_path_s;
+            }
+            bool too_close = false;
+          
+            for (int i=0; i<sensor_fusion.size(); i++)
+            {
+              //car is in my lane
+              float d = sensor_fusion[i][6];
+              if(d< (2+4*lane+2) && d> (2+4*lane-2)  )
+              {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx + vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+                check_car_s += ((double)prev_size*0.02*check_speed );
+                if( (check_car_s>car_s ) && (check_car_s-car_s< 30  ) )
+                   {
+                     //take action
+                     ref_vel =29.5;
+                     //too_close = true;
+                   }
+       
+              }
+            }
+          
+            if( too_close = true)
+            {
+              ref_vel -= .224;
+            }
+          //---
 
             if(prev_size < 2)
             {
